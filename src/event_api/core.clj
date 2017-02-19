aae316543740cd81f75ed6514c301e72f1846f4a
Eugene Hardbread <agnivalent@gmail.com>
2017-02-19 14:13:04 -0300

    prepared for heroku

--- src/event_api/core.clj
+++ src/event_api/core.clj
@@ -1,23 +1,15 @@
 (ns event-api.core
   (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
             [compojure.handler :refer [site]]
-            [compojure.route :as route]
-            [clojure.java.io :as io]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
+            [ring.util.response :refer [response]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.json :refer [wrap-json-response]]))
 
-(defn splash []
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body {:keke "kdsaoko"}})
-
 (defroutes app
   (GET "/" []
-       (splash))
-  (ANY "*" []
-       (route/not-found (slurp (io/resource "404.html")))))
+       (response {:keke "kdsadoko23"})))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
