87876fecf10aae03248eb5c19624210704b5b016
Eugene Hardbread <agnivalent@gmail.com>
2017-02-19 13:11:01 -0300

    ring now works (jetty), helloworld works

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-jetty-adapter "1.5.1"]
+                 ;;[http-kit "2.2.0"]
+                 [compojure "1.5.2"]
+                 [environ "1.1.0"]]
+  :plugins [[lein-environ "1.1.0"]]
+  :min-lein-version "2.0.0"
   :main ^:skip-aot event-api.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :uberjar-name "event-api-standalone.jar"
+  :profiles {:production {:env {:production true}}})
